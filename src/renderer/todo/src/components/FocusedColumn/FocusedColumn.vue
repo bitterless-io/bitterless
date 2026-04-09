2a1a231623106fdb91f90f2876bf6e1aa7de1adc
ral <ralwayne@163.com>
2026-04-09 22:39:45 +0800

    nm

--- src/renderer/todo/src/components/FocusedColumn/FocusedColumn.vue
+++ src/renderer/todo/src/components/FocusedColumn/FocusedColumn.vue
@@ -1,45 +1,38 @@
 <template>
   <div class="focused-column">
     <div class="focused-column__header">
-      <span class="focused-column__header-title">{{ i18nHelper.todo.focusedDomain }}</span>
-      <span v-if="todoStore.focusedTodoList.length > 0" class="focused-column__header-count">
-        {{ todoStore.focusedTodoList.length }}
-      </span>
-      <a-dropdown trigger="click" position="br" @select="onFilterSelect">
-        <a-button
-          class="focused-column__filter-btn"
-          :class="{ 'focused-column__filter-btn--active': isFiltered }"
-          size="mini"
-          type="text"
-        >
-          <template #icon>
-            <icon-filter :size="14" />
-          </template>
-        </a-button>
-        <template #content>
-          <a-doption value="important">
-            <template #icon>
-              <icon-check v-if="todoSettingStore.focusedFilters.important" :size="12" />
-              <span v-else class="focused-column__filter-placeholder" />
-            </template>
-            {{ i18nHelper.todo.focusedFilterImportant }}
-          </a-doption>
-          <a-doption value="overdue">
-            <template #icon>
-              <icon-check v-if="todoSettingStore.focusedFilters.overdue" :size="12" />
-              <span v-else class="focused-column__filter-placeholder" />
-            </template>
-            {{ i18nHelper.todo.focusedFilterOverdue }}
-          </a-doption>
-          <a-doption value="today">
-            <template #icon>
-              <icon-check v-if="todoSettingStore.focusedFilters.today" :size="12" />
-              <span v-else class="focused-column__filter-placeholder" />
-            </template>
-            {{ i18nHelper.todo.focusedFilterToday }}
-          </a-doption>
-        </template>
-      </a-dropdown>
+      <div class="focused-column__header-title-row">
+        <span class="focused-column__header-title">{{ i18nHelper.todo.focusedDomain }}</span>
+        <span v-if="todoStore.focusedTodoList.length > 0" class="focused-column__header-count">
+          {{ todoStore.focusedTodoList.length }}
+        </span>
+      </div>
+      <div class="focused-column__filter-row">
+        <label class="focused-column__filter-item">
+          <a-checkbox
+            :model-value="todoSettingStore.focusedFilters.important"
+            size="mini"
+            @change="(v: boolean) => toggleFilter('important', v)"
+          />
+          <span class="focused-column__filter-label">{{ i18nHelper.todo.focusedFilterImportant }}</span>
+        </label>
+        <label class="focused-column__filter-item">
+          <a-checkbox
+            :model-value="todoSettingStore.focusedFilters.today"
+            size="mini"
+            @change="(v: boolean) => toggleFilter('today', v)"
+          />
+          <span class="focused-column__filter-label">{{ i18nHelper.todo.focusedFilterToday }}</span>
+        </label>
+        <label class="focused-column__filter-item">
+          <a-checkbox
+            :model-value="todoSettingStore.focusedFilters.overdue"
+            size="mini"
+            @change="(v: boolean) => toggleFilter('overdue', v)"
+          />
+          <span class="focused-column__filter-label">{{ i18nHelper.todo.focusedFilterOverdue }}</span>
+        </label>
+      </div>
     </div>
     <div class="focused-column__body" ref="bodyRef" @scroll="onBodyScroll">
       <div class="focused-column__todo-list">
@@ -63,8 +56,8 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import { IconArrowUp, IconFilter, IconCheck } from '@arco-design/web-vue/es/icon';
+import { ref } from 'vue';
+import { IconArrowUp } from '@arco-design/web-vue/es/icon';
 import TodoRow from '../TodoRow/TodoRow.vue';
 import { todoStore } from '../../store/todo.store';
 import { todoSettingStore } from '../../store/todoSetting.store';
@@ -74,11 +67,6 @@ import { i18nHelper } from '@renderer/common/i18n/i18n.helper';
 const bodyRef = ref<HTMLElement | null>(null);
 const showBackToTop = ref(false);
 
-const isFiltered = computed(() => {
-  const f = todoSettingStore.focusedFilters;
-  return !f.important || !f.overdue || !f.today;
-});
-
 const onBodyScroll = () => {
   showBackToTop.value = (bodyRef.value?.scrollTop ?? 0) > 150;
 };
@@ -87,10 +75,8 @@ const scrollToTop = () => {
   bodyRef.value?.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
-const onFilterSelect = (value: string | number | Record<string, any>) => {
-  const key = value as keyof FocusedFilters;
-  const current = todoSettingStore.focusedFilters;
-  todoSettingStore.setFocusedFilters({ ...current, [key]: !current[key] });
+const toggleFilter = (key: keyof FocusedFilters, value: boolean) => {
+  todoSettingStore.setFocusedFilters({ ...todoSettingStore.focusedFilters, [key]: value });
 };
 </script>
 
